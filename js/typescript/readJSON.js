e4f03567f1b8bc0366a357259107e0d972a066e4
Elizaveta <liza_fahrut@mail.ru>
2022-06-19 19:17:57 +1000

    все работает

--- js/typescript/readJSON.js
+++ js/typescript/readJSON.js
@@ -1,7 +1,56 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const motorists_json_1 = __importDefault(require("../JSONfiles/motorists.json"));
-console.log(motorists_json_1.default);
+import data from '../JSONfiles/motorists.json' assert { type: 'json' };
+var tableBrieftbody = document.getElementById("tableBrief_tbody");
+var personCell = document.getElementById("person");
+var manufacturerCell = document.getElementById("manufacturer");
+var modelCell = document.getElementById("model");
+var yearCell = document.getElementById("year");
+var typeCell = document.getElementById("type");
+var colorCell = document.getElementById("color");
+var isConvertibleCell = document.getElementById("isConvertible");
+var vinCell = document.getElementById("vin");
+var buttonDelete = document.getElementById('delete-btn');
+var hidenBlock = document.getElementById("main__detail");
+hidenBlock.hidden = true;
+var idHuman;
+createTableBrief();
+tableBrieftbody.addEventListener('click', function (event) {
+    var target = event.target;
+    var tar = event === null || event === void 0 ? void 0 : event.target;
+    var row = target.closest('tr');
+    idHuman = data[row.rowIndex - 1].id;
+    var extendedInfo = data.filter(function (item) { return item.id === idHuman; });
+    console.log(extendedInfo);
+    extendedInfo.forEach(function (item) {
+        personCell.textContent = item.person.firstname + " " + item.person.lastname;
+        manufacturerCell.textContent = item.car.manufacturer;
+        modelCell.textContent = item.car.model;
+        yearCell.textContent = item.car.year.toString();
+        typeCell.textContent = item.car.model;
+        colorCell.bgColor = item.car.color;
+        isConvertibleCell.checked = item.car.isConvertible;
+        vinCell.textContent = item.car.vin;
+    });
+    hidenBlock.hidden = false;
+});
+buttonDelete.addEventListener('click', function (event) {
+    var numberOfdata = 0;
+    data.forEach(function (item) {
+        if (item.id == idHuman) {
+            numberOfdata = data.indexOf(item);
+        }
+    });
+    data.splice(numberOfdata, 1);
+    hidenBlock.hidden = true;
+    var tr = document.createElement('tr');
+    while (tableBrieftbody.rows[0]) {
+        tableBrieftbody.deleteRow(0);
+    }
+    createTableBrief();
+});
+function createTableBrief() {
+    data.forEach(function (member) {
+        var tr = document.createElement('tr');
+        tr.innerHTML = "<td>".concat(member.person.firstname, " ").concat(member.person.lastname, "</td>\n                      <td>").concat(member.car.manufacturer, "</td>\n                      <td>").concat(member.car.model, "</td>\n                      <td>").concat(member.car.year, "</td>");
+        tableBrieftbody.appendChild(tr);
+    });
+}
--- typescript/readJSON.ts
+++ typescript/readJSON.ts
@@ -1,5 +1,71 @@
 
-import data from '../JSONfiles/motorists.json';
+import data from '../JSONfiles/motorists.json' assert { type: 'json' };
 
-console.log(data);
+const tableBrieftbody: HTMLTableElement = <HTMLTableElement>document.getElementById("tableBrief_tbody");
+
+const personCell: HTMLTableCellElement = <HTMLTableCellElement>document.getElementById("person");
+const manufacturerCell: HTMLTableCellElement = <HTMLTableCellElement>document.getElementById("manufacturer");
+const modelCell: HTMLTableCellElement = <HTMLTableCellElement>document.getElementById("model");
+const yearCell: HTMLTableCellElement = <HTMLTableCellElement>document.getElementById("year");
+const typeCell: HTMLTableCellElement = <HTMLTableCellElement>document.getElementById("type");
+const colorCell: HTMLTableCellElement = <HTMLTableCellElement>document.getElementById("color");
+const isConvertibleCell: HTMLInputElement = <HTMLInputElement>document.getElementById("isConvertible");
+const vinCell: HTMLTableCellElement = <HTMLTableCellElement>document.getElementById("vin");
+
+const buttonDelete: HTMLButtonElement = <HTMLButtonElement>document.getElementById('delete-btn');
+
+const hidenBlock: HTMLElement = <HTMLElement>document.getElementById("main__detail");
+hidenBlock.hidden = true;
+
+let idHuman: string;
+
+createTableBrief();
+
+tableBrieftbody.addEventListener('click', (event: Event) => {
+    const target: HTMLTableRowElement = <HTMLTableRowElement>event.target;
+    let tar = event?.target as HTMLElement;
+    
+    const row: HTMLTableRowElement = <HTMLTableRowElement>target.closest('tr');
+    idHuman = data[row.rowIndex - 1].id;
+    const extendedInfo = data.filter(item => item.id === idHuman)
+    console.log(extendedInfo);
+    extendedInfo.forEach(item => {
+        personCell.textContent = item.person.firstname + " " + item.person.lastname;
+        manufacturerCell.textContent = item.car.manufacturer;
+        modelCell.textContent = item.car.model;
+        yearCell.textContent = item.car.year.toString();
+        typeCell.textContent = item.car.model;
+        colorCell.bgColor = item.car.color;
+        isConvertibleCell.checked = item.car.isConvertible;
+        vinCell.textContent = item.car.vin;
+    })
+    hidenBlock.hidden = false;   
+});
+buttonDelete.addEventListener('click', event => {
+    let numberOfdata = 0;
+    data.forEach(item => {
+        if (item.id == idHuman) {
+            numberOfdata = data.indexOf(item);
+        }
+    });
+
+    data.splice(numberOfdata, 1);
+    hidenBlock.hidden = true;
+    const tr: HTMLTableRowElement = <HTMLTableRowElement>document.createElement('tr');
+    while (tableBrieftbody.rows[0]) {
+        tableBrieftbody.deleteRow(0);
+    }
+    createTableBrief();
+});
+
+function createTableBrief(): void {
+    data.forEach(member => {
+        const tr: HTMLTableRowElement = document.createElement('tr');
+        tr.innerHTML = `<td>${member.person.firstname} ${member.person.lastname}</td>
+                      <td>${member.car.manufacturer}</td>
+                      <td>${member.car.model}</td>
+                      <td>${member.car.year}</td>`
+        tableBrieftbody.appendChild(tr);
+    });
+}
 
